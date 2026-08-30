64ec4f9a57d1af7934ba4f0a7fc99e047d98e62e
rj9884 <rj.vidyagyan@gmail.com>
2026-08-31 00:01:46 +0530

    feat(matugen): add editor color templates (nvim, zed, vscode, sourceview)

---
+++ config/matugen/templates/nvim.lua
@@ -0,0 +1,17 @@
+-- Matugen generated colors for Neovim
+local M = {}
+
+M.colors = {
+    bg        = "{{colors.surface.default.hex}}",
+    fg        = "{{colors.on_surface.default.hex}}",
+    primary   = "{{colors.primary.default.hex}}",
+    secondary = "{{colors.secondary.default.hex}}",
+    tertiary  = "{{colors.tertiary.default.hex}}",
+    error     = "{{colors.error.default.hex}}",
+    surface   = "{{colors.surface_container.default.hex}}",
+    outline   = "{{colors.outline.default.hex}}",
+    on_primary = "{{colors.on_primary.default.hex}}",
+    on_secondary = "{{colors.on_secondary.default.hex}}",
+}
+
+return M
